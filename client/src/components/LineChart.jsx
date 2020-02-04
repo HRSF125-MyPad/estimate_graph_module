c45a1ae7973c577489806e807c6463ed9e843273
Noel Mendoza <nmendozavd@gmail.com>
2020-02-03 19:34:02 -0800

    refactored components

--- client/src/components/LineChart.jsx
+++ client/src/components/LineChart.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
-  
+const Chart = styled.div`
+ 
   svg {
     background: white;
     width: 670px;
     height: 264px;
   
+    polyline {
+      fill: transparent;
+      stroke: rgb(51, 51, 51);
+      stroke-width: 2;
+      opacity: 1;
+    } 
+
     g {
-     
+      
       line {
+        color: #999;
         stroke: rgb(226, 226, 226);
         fill: transparent;
         stroke-width: 1;
@@ -18,7 +26,6 @@ const Wrapper = styled.div`
 
         text {
           text-anchor: "middle";
-          color: #999999;
           font-size: 14px;
           /* fill: rgb(153, 153, 153); */
           font-family: Libre Franklin;
@@ -28,14 +35,11 @@ const Wrapper = styled.div`
           stroke-width: 0;
         }
       }
-
-    }
-   
+    } 
  }
 `
 
-
-class LineChart5Year extends React.Component {
+class LineChart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -44,35 +48,52 @@ class LineChart5Year extends React.Component {
 
   render() {
     return (
-      <Wrapper>
+      <Chart>
         <svg viewBox="0 0 670 264">
-          <g>
-            {/* parallel lines in graph*/}
-            <line x1="15" x2="610" y1="214" y2="214"></line>
-            <line x1="610" x2="15" y1="178" y2="178"></line>
-            <line x1="610" x2="15" y1="133" y2="133"></line>
-            <line x1="610" x2="15" y1="88" y2="88"></line>
-            <line x1="610" x2="15" y1="43" y2="43"></line>
+            <g>
+              {/* parallel lines in graph*/}
+              <line x1="15" x2="610" y1="214" y2="214"></line>
+              <line x1="610" x2="15" y1="178" y2="178"></line>
+              <line x1="610" x2="15" y1="133" y2="133"></line>
+              <line x1="610" x2="15" y1="88" y2="88"></line>
+              <line x1="610" x2="15" y1="43" y2="43"></line>
+              
+              {/* labels on x-axis */}
+              <text dx="0" dy="11.9" x="16" y="226">Jan</text>
+              <text dx="0" dy="11.9" x="112" y="226">Mar</text>
+              <text dx="0" dy="11.9" x="210" y="226">May</text>
+              <text dx="0" dy="11.9" x="309" y="226">Jul</text>
+              <text dx="0" dy="11.9" x="409" y="226">Sept</text>
+              <text dx="0" dy="11.9" x="508" y="226">Nov</text>
+              <text dx="0" dy="11.9" x="606" y="226">Jan</text>
 
-            {/* labels on x-axis */}
-            <text dx="0" dy="11.9" x="15" y="226">2015</text>
-            <text dx="0" dy="11.9" x="134" y="226">2016</text>
-            <text dx="0" dy="11.9" x="253" y="226">2017</text>
-            <text dx="0" dy="11.9" x="371" y="226">2018</text>
-            <text dx="0" dy="11.9" x="490" y="226">2019</text>
-            <text dx="0" dy="11.9" x="609" y="226">2020</text>
-
-            {/* labels on y-axis */}
-            <text dx="0" dy="4.97" x="622" y="43">$3.0M</text>
-            <text dx="0" dy="4.97" x="622" y="88">$2.5M</text>
-            <text dx="0" dy="4.97" x="622" y="133">$2.0M</text>
-            <text dx="0" dy="4.97" x="622" y="178">$1.5M</text>
-          </g>
-        </svg>
-      </Wrapper>
+              {/* labels on y-axis */}
+              <text dx="0" dy="4.97" x="622" y="43">$3.0M</text>
+              <text dx="0" dy="4.97" x="622" y="88">$2.5M</text>
+              <text dx="0" dy="4.97" x="622" y="133">$2.0M</text>
+              <text dx="0" dy="4.97" x="622" y="178">$1.5M</text>
+            </g>
+            
+            <polyline points="
+              20,90
+              80,100
+              130,120
+              180,130
+              230,140
+              280,150
+              320,140
+              370,145
+              420,160
+              470,155
+              550,165
+              610,150
+            "
+            /> 
+          </svg>
+      </Chart>
     )
   }
 
 }
 
-export default LineChart5Year;
+export default LineChart;
